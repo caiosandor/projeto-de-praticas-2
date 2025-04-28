d1bd39c0ae042948bdb05f57a31d796f2c592c57
Caio Sandor Jambor Oliveira <146789084+caiosandor@users.noreply.github.com>
2025-04-28 16:01:05 -0300

    Delete mapa.js

--- mapa.js
+++
@@ -1,8 +0,0 @@
-let mapInstance;
-
-function initMap() {
-    mapInstance = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -22.9194, lng: -42.8185 }, 
-        zoom: 12,
-    });
-}
